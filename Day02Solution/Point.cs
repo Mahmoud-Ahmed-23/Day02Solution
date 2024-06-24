efbc2963553ade85cd17f89e203c2c9e656dc009
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-06-24 16:35:03 +0300

    Add project files.

---
+++ Day02Solution/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02Solution
+{
+    internal class Point
+    {
+        public int number1;
+        public int number2;
+    }
+}
---
+++ Day02Solution/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Principal;
+
+namespace Day02Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region 1-  Write a program that allows the user to enter a number then print it.
+
+
+            //Console.Write("Please Enter a Number: ");
+
+            //int number = int.Parse(Console.ReadLine() ?? "0");
+
+            //Console.WriteLine($"number is: {number}");
+
+            #endregion
+            
+
+        }
+    }
+}
